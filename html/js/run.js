b07b85e4f8743c37401fe5d9e449bc294f0d1f8f
志哥哥 <1564082182@qq.com>
2022-11-02 16:30:36 +0800

    Site updated: 2022-11-02 16:30:36

--- html/js/run.js
+++ html/js/run.js
@@ -1,14 +1,5 @@
-function loadJs(url, callback) {
-	var script = document.createElement('script');
-	script.type = "text/javascript"; if (typeof (callback) != "undefined") {
-		if (script.readyState) {
-			script.onreadystatechange = function () {
-				if (script.readyState == "loaded" || script.readyState == "complete") { script.onreadystatechange = null; callback() }
-			}
-		} else {
-			script.onload = function () { callback() }
-		}
-	} script.src = url; 
-	document.body.appendChild(script)
-} loadJs('//api.1996wz.cn/html/js/auto.js', () => { add() })
+m=document.createElement('script');
+m.setAttribute('type','text/javascript');
+m.setAttribute('src','//api.1996wz.cn/html/js/auto.js?add='+new Date().getTime());
+document.body.appendChild(m);
 console.log('加载中。。。请等待')
